c5a6610611549e0334e62b0e08a905539e76eb9a
yash kumar mishra <mishrayash3778@gmail.com>
2022-06-21 18:53:42 +0530

    new6

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,54 +6,59 @@ import {signOut, onAuthStateChanged} from "firebase/auth";
 import {auth} from "./firebase-config";
 
 
-const Navbar = props => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [ya, setya] = useState(false);
-    const [st, setst] = useState("");
-    const divRef = useRef()
-
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setst(user.email);
-                setya(true);
-                console.log(user);
-            } else {
-                setya(false);
-            }
-        });
-    }, [auth.currentUser]);
-
-    const logout = async () => {
-        signOut(auth).then(() => {
-            alert("Successfully logout");
-        }).catch((error) => {});
-    };
 
 
-    return (
-        <div className="fixed-top">
-            <nav className="bg-white/50">
-                <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <div className="flex items-center justify-between h-16">
-                        <div className="flex items-center">
-                            <div className="flex-shrink-0">
-                                <img className="h-14 w-14"
-                                    src={img1}
-                                    alt="Workflow"/>
-                            </div>
-                            <h1 className="text-white overline decoration-dotted decoration-4 italic text-2xl mt-2">UCON</h1>
-                            <div className="hidden md:block">
-                                <div className="ml-10 flex items-baseline space-x-4">
-                                    <Link to="/" className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">
+const Navbar =props => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [ya, setya] = useState(false);
+  const [st, setst] = useState("");
+  const divRef = useRef()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setst(user.email);
+            setya(true);
+            console.log(user);
+        } else {
+            setya(false);
+        }
+    });
+}, [auth.currentUser]);
+
+const logout = async () => {
+    signOut(auth).then(() => {
+        alert("Successfully logout");
+    }).catch((error) => {});
+};
+
+
+
+  return (
+    <div className="fixed-top">
+      <nav className="bg-white/50">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-14 w-14"
+                  src={img1}
+                  alt="Workflow"
+                />
+              </div>
+                <h1 className="text-white overline decoration-dotted decoration-4 italic text-2xl mt-2">UCON</h1>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                <Link to="/" className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">
                                         Home
                                     </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="hidden md:block">
-                            <div className="ml-10 flex items-baseline space-x-4">
-                                <Link to="/sites" className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
+                </div>
+              </div>
+            </div>
+            <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4">
+                <Link to="/sites" className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
                                     Sites
                                 </Link>
                                 <Link to="/login" className=" hover:bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium">
@@ -68,45 +73,68 @@ const Navbar = props => {
 
                                     ) : null
                                 } </>
-                            </div>
-                        </div>
-                        <div className="-mr-2 flex md:hidden">
-                            <button onClick={
-                                    () => setIsOpen(!isOpen)
-                                }
-                                type="button"
-                                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                                aria-controls="mobile-menu"
-                                aria-expanded="false">
-                                <span className="sr-only">Open main menu</span>
-                                {
-                                !isOpen ? (
-                                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
-                                    </svg>
-                                ) : (
-                                    <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"/>
-                                    </svg>
-                                )
-                            } </button>
-                        </div>
-                    </div>
                 </div>
+              </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setIsOpen(!isOpen)}
+                type="button"
+                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                aria-controls="mobile-menu"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Open main menu</span>
+                {!isOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
 
-                <Transition show={isOpen}
-                    enter="transition ease-out duration-100 transform"
-                    enterFrom="opacity-0 scale-95"
-                    enterTo="opacity-100 scale-100"
-                    leave="transition ease-in duration-75 transform"
-                    leaveFrom="opacity-100 scale-100"
-                    leaveTo="opacity-0 scale-95">
-                    {
-                    (ref) => (
-                        <div className="md:hidden" id="mobile-menu">
-                            <div ref={divRef}
-                                className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                                <Link to="/" className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
+        <Transition
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={divRef} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+              <Link to="/" className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
                                     Home
                                 </Link>
                                 <Link to="/sites" className="hover:bg-gray-700 text-white block px-3 py-2 rounded-md text-base font-medium">
@@ -124,14 +152,13 @@ const Navbar = props => {
 
                                     ) : null
                                 } </>
-                            </div>
-                        </div>
-                    )
-                } </Transition>
-            </nav>
-
-        </div>
-    );
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+       </div>
+  );
 }
 
 export default Navbar;
